cbd314f38360246b38814283c7929940709d995c
Muhammad Iqbal <iqbal.adudu@gmail.com>
2025-03-26 14:02:25 +0200

    fix

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -9,8 +9,6 @@ import React from "react";
 import { importMap } from "./admin/importMap.js";
 import "./custom.scss";
 
-export const runtime = "edge";
-
 type Args = {
   children: React.ReactNode;
 };
