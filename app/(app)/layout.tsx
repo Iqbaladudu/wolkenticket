4d02a5a415b1dae4fb7b6c8c8bf43c2433fb3b12
Muhammad Iqbal <iqbal.adudu@gmail.com>
2025-04-25 10:50:07 +0300

    gtm

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
             <SpeedInsights />
           </CheckoutFormProvider>
         </QueryProvider>
-        <Script>
+        <Script id="clarity">
           {`(function(c,l,a,r,i,t,y){
                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
